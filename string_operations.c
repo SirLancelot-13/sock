9581ce75ab4fb2265e77621172cd300615dfe339
SirLancelot-13 <pragyansrivastava13@gmail.com>
2026-06-28 19:05:37 +0530

    Changed the idea of diverting from RFC to using RFC-based conventions

--- string_operations.c
+++ string_operations.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 
-char *send_get_request(char *host, char *endpoint, char *content){
+char *send_post_request(char *host, char *endpoint, char *content){
     char *req=malloc(1024);
-    sprintf(req, "GET %s HTTP/1.1\r\nHost: %s\r\nConnection: keep-alive\r\nContent-Type: application/xml\r\nContent-length: %d\r\n%s\r\n\r\n",endpoint,host,strlen(content),content);
-    return *req;
+    snprintf(req, "POST %s HTTP/1.1\r\nHost: %s\r\nConnection: keep-alive\r\nContent-Type: application/xml\r\nContent-Length: %d\r\n%s\r\n\r\n",endpoint,host,strlen(content),content);
+    return req;
 }
 
 char *encode_response_text(char *response_text){
