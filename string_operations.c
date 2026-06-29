c94f0db0c36113c35e283d8d8644bd23fa7455c0
SirLancelot-13 <pragyansrivastava13@gmail.com>
2026-06-29 08:59:37 +0530

    Add get request and response builder

--- string_operations.c
+++ string_operations.c
@@ -7,6 +7,14 @@ char *send_post_request(char *host, char *endpoint, char *content){
     return req;
 }
 
-char *encode_response_text(char *response_text){
+char *send_get_request(char *host, char *endpoint){
+    char *req=malloc(1024);
+    snprintf(req, "GET %s HTTP/1.1\r\nHost: %s\r\nConnection: keep-alive\r\n\r\n",endpoint,host);
+    return req;
+}
 
+char *encode_get_response_text(char *content){
+    char *res=malloc(1024);
+    snprintf("HTTP/1.1 200 OK\r\nCache-Control: no-cache\r\nConnection: keep-alive\r\nContent-Type: application/xml\r\nContent-Length: %d\r\n%s",strlen(content),content);
+    return res;
 }
