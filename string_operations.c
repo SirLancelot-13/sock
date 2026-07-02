1d1ef6f8962943e25b424e33512aea9107b030e2
SirLancelot-13 <pragyansrivastava13@gmail.com>
2026-07-02 12:07:33 +0530

    Added mock get and post requests

--- string_operations.c
+++ string_operations.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 char *send_post_request(char *host, char *endpoint, char *content){
     char *req=malloc(1024);
-    snprintf(req, 1024, "POST %s HTTP/1.1\r\nHost: %s\r\nConnection: keep-alive\r\nContent-Type: application/xml\r\nContent-Length: %d\r\n%s\r\n\r\n",endpoint,host,strlen(content),content);
+    snprintf(req, 1024, "POST %s HTTP/1.1\r\nHost: %s\r\nConnection: keep-alive\r\nContent-Type: application/xml\r\nContent-Length: %zu\r\n%s\r\n\r\n",endpoint,host,strlen(content),content);
     return req;
 }
 
@@ -15,6 +16,6 @@ char *send_get_request(char *host, char *endpoint){
 
 char *encode_get_response_text(char *content){
     char *res=malloc(1024);
-    snprintf(res, 1024, "HTTP/1.1 200 OK\r\nCache-Control: no-cache\r\nConnection: keep-alive\r\nContent-Type: application/xml\r\nContent-Length: %d\r\n%s",strlen(content),content);
+    snprintf(res, 1024, "HTTP/1.1 200 OK\r\nCache-Control: no-cache\r\nConnection: keep-alive\r\nContent-Type: application/xml\r\nContent-Length: %zu\r\n%s",strlen(content),content);
     return res;
 }
---
+++ string_operations.h
@@ -0,0 +1,10 @@
+#ifndef string_operations
+#define string_operations
+
+char *send_post_request(char *host, char *endpoint, char *content){}
+
+char *send_get_request(char *host, char *endpoint){}
+
+char *encode_get_response_text(char *content){}
+
+#endif
