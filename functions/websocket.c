ff44dd4142ca5848e41c782e2cb011be3381467a
SirLancelot-13 <pragyansrivastava13@gmail.com>
2026-07-15 21:57:12 +0530

    Completed a basic websocket implementation with SHA1 encryption

--- functions/websocket.c
+++ functions/websocket.c
@@ -4,7 +4,8 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdio.h>
-
+#include <string.h>
+#include "crypt.h"
 int read_websocket_frame(int fd, char *payload_o, size_t maxlen, int *optcode_out){
     uint8_t header[2];
     if (recv(fd, header, 2, 0) <= 0){
@@ -56,6 +57,56 @@ int read_websocket_frame(int fd, char *payload_o, size_t maxlen, int *optcode_ou
     return (int)payload_len;
 }
 
-void send_ws_text_frame({
+void send_ws_frame(int fd, const char *http_req){
+    size_t len=strlen(http_req);
+    uint8_t header[10];
+    header[0]=0x81; //FIN bit set, text-frame
+    int header_len=2;
+    if (len<126){
+        header[1]=len;
+    } else if (len<65536) {
+        header[1]=126;
+        header[2]=(len>>8)&0xFF;
+        header[3]=len&0xFF;
+        header_len+=2;
+    } else{
+        header[1]=127;
+        header_len+=8;
+        for (int i=0; i<8; i++){
+            header[2+i]=(len>>(56-8*i))&0xFF;
+        }
+    }
+    send(fd, header, header_len, 0);
+    send(fd, http_req, len, 0);
+}
 
-})
+int handle_ws_frame(int fd, const char *http_req){
+    const char *key_hdr="Sec Web-Socket-Key: ";
+    char *key_start=strstr(key_hdr, http_req);
+    if (!key_start) perror("Failed to parse in the start of the http-request.\n");
+    char *key_end=strstr(http_req, "\r\n");
+    if (!key_end) perror("Failed to parse in the end of the http-request.\n");
+    size_t key_len=key_end-key_start-strlen(key_hdr);
+    char key[128];
+    strncpy(key, key_start+strlen(key_hdr), key_len);
+    key[key_len]='\0';
+    // Concatenate key with GUID.
+    char concatenate[256];
+    snprintf(concatenate, sizeof(concatenate), "%s%s", key, "258EAFA5-E914-47DA-95CA-C5AB0DC85B11");
+    // Hash ts
+    uint8_t hash[20];
+    sha1_hash((const int8_t *)concatenate, strlen(concatenate), hash);
+    // Convert to b64
+    char b64[32];
+    base64_encode(hash, 20, b64);
+    // Send response
+    char response[512];
+    snprintf(response, sizeof(response),
+             "HTTP/1.1 101 Switching Protocols\r\n"
+             "Upgrade: websocket\r\n"
+             "Connection: Upgrade\r\n"
+             "Sec-WebSocket-Accept: %s\r\n"
+             "\r\n", b64);
+    send(fd, response, strlen(response), 0);
+    return 0;
+}
